8a19ca38906bd9a3afbdf884dfe79c0f04710aa3
srikari malladi <127287195+SrikariMalladi@users.noreply.github.com>
2023-07-08 19:28:53 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String name;
+    private int rollNumber;
+    private String grade;
+
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+}
